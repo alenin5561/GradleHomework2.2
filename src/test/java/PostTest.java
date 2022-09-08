1a15934f056551a5235beafa72cb134c127d37d0
Аленин Андрей <aleninmailbox@gmail.com>
2022-09-08 11:37:51 +0300

    test

--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -10,14 +10,14 @@ public class PostTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("user Name Andrew") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("user name Andrew") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("user name Alex"))
+                .body("data", equalTo("user name Andrew"))
         ;
     }
 }
